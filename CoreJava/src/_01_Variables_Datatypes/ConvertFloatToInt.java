930a183286a751359569086c0a73c34fd90d2a00
QmesR <60589484+QmesR@users.noreply.github.com>
2020-02-04 23:07:40 -0500

    Update ConvertFloatToInt.java

--- CoreJava/src/_01_Variables_Datatypes/ConvertFloatToInt.java
+++ CoreJava/src/_01_Variables_Datatypes/ConvertFloatToInt.java
@@ -7,3 +7,19 @@ public class ConvertFloatToInt {
 
     }
 }
+
+____________________________________________________________________
+public class ConvertFloatToInt {
+
+	public static void main(String[] args){
+		
+	
+	float f = 10.5f;
+	// typecast the above float to integer and print the output.
+	
+	int b = (int) f;
+	
+	System.out.println(f); // prints 10.5
+	System.out.println(b); // prints 10
+	}
+}
