19d3f36d1cfd35f51702848c3d63412f66cd1d05
QmesR <60589484+QmesR@users.noreply.github.com>
2020-02-04 21:56:40 -0500

    Update CheckEvenOdd.java

--- CoreJava/src/_01_Variables_Datatypes/CheckEvenOdd.java
+++ CoreJava/src/_01_Variables_Datatypes/CheckEvenOdd.java
@@ -2,6 +2,7 @@ package _01_Variables_Datatypes;
 
 import org.w3c.dom.ls.LSOutput;
 
+
 /**
  * Use scanner to take input dynamically.
  */
@@ -12,5 +13,27 @@ public class CheckEvenOdd {
 
         boolean isOdd = false;// use the module operator to check if a number is odd or even.(Google it)
         System.out.println("The number entered "+ num +" is Odd ? "+ isOdd);
+    }
+}
+__________________________________________________________________
+import java.util.Scanner;
+/**
+ * 
+ * @author umesr
+ * Use scanner to take input dynamically.
+ */
+public class CheckEvenOdd {
+	public static void main(String[] args) {
+		int x =0;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter an Integer number:");
+		x = sc.nextInt();
+		
+		if (x % 2 == 0) 
+			System.out.println("Your number is EVEN.");
+			
+		else 
+			System.out.println("Your number is ODD.");
     }
 }
